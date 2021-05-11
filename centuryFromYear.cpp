30240b477b824d4b6daae2fe7be4f71ed10f9654
Brianna Scott <44863228+simplybrix@users.noreply.github.com>
2021-05-11 13:48:41 -0400

    Create centuryFromYear.cpp

---
+++ centuryFromYear.cpp
@@ -0,0 +1,13 @@
+//Code Signal Arcade
+//Given a year, return the century it is in.
+//The first century spans from the year 1 up to and including the year 100, 
+//the second - from the year 101 up to and including the year 200, etc.
+
+int centuryFromYear(int year) {
+ int centuryCount = 0;
+ while(year > 0){
+     year = year - 100;
+     centuryCount = centuryCount + 1;
+ }
+ return centuryCount;
+}
