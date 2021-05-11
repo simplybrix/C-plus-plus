03bd497da222e0e8b418e6da69005293037926fe
Brianna Scott <44863228+simplybrix@users.noreply.github.com>
2021-05-11 14:17:30 -0400

    Update adjacentElementsProduct.cpp

--- adjacentElementsProduct.cpp
+++ adjacentElementsProduct.cpp
@@ -1,3 +1,19 @@
 //Code Signal Arcade
 //Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
+int adjacentElementsProduct(vector<int> inputArray) {
+
+int biggestElementProduct = -1001;
+    
+    for (int i = 0; i < inputArray.size() - 1; i++) {
+        int firstNum = inputArray[i];
+        int secondNum = inputArray[i+1];
+        int currentProd = firstNum * secondNum;
+        
+        if (currentProd > biggestElementProduct) {
+            biggestElementProduct = currentProd;
+        }
+    }
+    
+    return biggestElementProduct;
+}
